ed708ad057dd1d7fb39081e8369ec85a411c085f
xiaoren"git config --global user.email 2998024535@qq.comgit config --global user.name xiaoren <2998024535@qq.com>
2023-03-01 23:48:52 +0800

    mvc运用postman5中传参方式@RequestParam

---
+++ SpringMvc_05/src/main/java/com/xiaoren/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.xiaoren.controller;
+
+import com.xiaoren.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    //普通参数：请求参数名与形参名不同时，使用@RequestParam注解关联请求参数名称与形参名称之间的关系
+    @RequestMapping("/commonParamDifferentName")
+    @ResponseBody
+    public String commonParamDifferentName(@RequestParam("name") String userName, int age) {
+        System.out.println("普通参数传递 userName ==> " + userName);
+        System.out.println("普通参数传递 age ==> " + age);
+        return "{'module':'common param different name'}";
+    }
+
+    //POJO参数：请求参数与形参对象中的属性对应即可完成参数传递
+    @RequestMapping("/pojoParam")
+    @ResponseBody
+    public String pojoParam(User user) {
+        System.out.println("pojo参数传递 user ==> " + user);
+        return "{'module':'pojo param'}";
+    }
+
+    //嵌套POJO参数：嵌套属性按照层次结构设定名称即可完成参数传递
+    @RequestMapping("/pojoContainPojoParam")
+    @ResponseBody
+    public String pojoContainPojoParam(User user){
+        System.out.println("pojo嵌套pojo参数传递 user ==> "+user);
+        return "{'module':'pojo contain pojo param'}";
+    }
+
+    //数组参数：同名请求参数可以直接映射到对应名称的形参数组对象中
+    @RequestMapping("/arrayParam")
+    @ResponseBody
+    public String arrayParam(@RequestParam String[] likes) {
+        System.out.println("数组参数传递 likes ==> " + Arrays.toString(likes));
+        return "{'module':'array param'}";
+    }
+
+    //集合参数：同名请求参数可以使用@RequestParam注解映射到对应名称的集合对象中作为数据
+    @RequestMapping("/listParam")
+    @ResponseBody
+    public String listParam(@RequestParam List<String> likes) {
+        System.out.println("集合参数传递 likes ==> " + likes);
+        return "{'module':'list param'}";
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
