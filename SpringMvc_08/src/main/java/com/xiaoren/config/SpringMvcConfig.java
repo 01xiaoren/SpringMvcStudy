8bae448b51f496915a73a408c5de844f27e58f76
xiaoren"git config --global user.email 2998024535@qq.comgit config --global user.name xiaoren <2998024535@qq.com>
2023-03-02 12:26:11 +0800

    mvc运用postman做响应

---
+++ SpringMvc_08/src/main/java/com/xiaoren/config/SpringMvcConfig.java
@@ -0,0 +1,13 @@
+package com.xiaoren.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@Configuration
+@ComponentScan("com.xiaoren.controller")
+//开启json数据类型自动转换
+@EnableWebMvc
+public class SpringMvcConfig {
+}
